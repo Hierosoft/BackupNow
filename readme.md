1d034dce349711c3fbfa971e8c03e46e4fa7406f
poikilos <7557867+poikilos@users.noreply.github.com>
2021-07-30 18:37:47 -0400

    Add Python 3 utf-8 handling, store args, &   (based on July 4, 2021 ronnymajani version).

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 # BackupNow
 Do a graphical backup based on ~/exclude.txt then ~/include.txt (using rsync which excludes or includes based on the first command switch, so --exclude-from is used first).
 
-rsync_with_progress.py is aerickson's 2011 version: <https://gist.github.com/aerickson/1283442>.
+rsync_with_progress.py is based on ronnymajani's July 4, 2021 comment on aerickson's 2011 version: <https://gist.github.com/aerickson/1283442> which is based on [Get total rsync progress using Python](https://libbits.wordpress.com/2011/04/09/get-total-rsync-progress-using-python/) by yegorich April 9, 2011.
 
 ## Related Projects
 - rsyncprogress: only accepts piped input
   - rsyncprogress is based on [rsyncprogress.py](https://gist.github.com/JohannesBuchner/4d61eb5a42aeaad6ce90) (`wget https://gist.github.com/JohannesBuchner/4d61eb5a42aeaad6ce90/raw/4bdecaabd3242931a79a9eeb55b4616916e054f9/rsyncprogress.py`) by JohannesBuchner
-  - A comment suggests using the progressbar-latest package with it.
+  - A comment suggests using the progressbar-latest package to fulfill the progressbar dependency.
--- rsync_with_progress.py
+++ rsync_with_progress.py
@@ -1,31 +1,39 @@
-# from https://libbits.wordpress.com/2011/04/09/get-total-rsync-progress-using-python/
-
 import subprocess
 import re
 import sys
 
 print('Dry run:')
-cmd = 'rsync -az --stats --dry-run ' + sys.argv[1] + ' ' + sys.argv[2]
+
+
+in_folder = sys.argv[1]
+out_folder = sys.argv[2]
+
+cmd = 'rsync -az --stats --dry-run ' + in_folder + ' ' + out_folder
+
 proc = subprocess.Popen(cmd,
     shell=True,
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
 )
-remainder = proc.communicate()[0]
-mn = re.findall(r'Number of files: (\d+)', remainder)
+
+output, err = proc.communicate()
+
+mn = re.findall(r'Number of files: (\d+)', output.decode('utf-8'))
 total_files = int(mn[0])
+
 print('Number of files: ' + str(total_files))
 
 print('Real rsync:')
-cmd = 'rsync -avz  --progress ' + sys.argv[1] + ' ' + sys.argv[2]
+
+cmd = 'rsync -avz  --progress ' + in_folder + ' ' + out_folder
 proc = subprocess.Popen(cmd,
     shell=True,
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
 )
+
 while True:
-    output = proc.stdout.readline()
-    if 'to-check' in output:
+    output = proc.stdout.readline().decode('utf-8')
     if 'to-check' in output:
         m = re.findall(r'to-check=(\d+)/(\d+)', output)
         progress = (100 * (int(m[0][1]) - int(m[0][0]))) / total_files
