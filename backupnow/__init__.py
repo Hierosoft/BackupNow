a7088f3ea6e66c037dce90b9a7442b7c6a84fe8d
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2024-06-11 15:48:10 -0400

    Improve comments.

--- backupnow/__init__.py
+++ backupnow/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
-This module is part of the BackupNow project by Jake "Poikilos" Gustafson 2021
+This module is part of the BackupNow project
+by Jake "Poikilos" Gustafson (c) 2021.
 You should have a copy of the license.txt file, otherwise see
-<https://github.com/poikilos/BackupNow/blob/main/license.txt>.
+<https://github.com/Poikilos/BackupNow/blob/main/license.txt>.
 '''
